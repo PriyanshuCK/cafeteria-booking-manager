5a72c0ad39d286c7eb92bfde0ab3a18b276221bc
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2024-12-16 16:51:41 +0530

    Add hero section and TodayMenu component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import TodayMenu from "@/components/TodayMenu";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default async function Home() {
   return (
     <>
-      <h1>Cafeteria Booking Manager</h1>
+      <div className="flex flex-col justify-center items-center h-64 sm:h-96 gap-2">
+        <h1 className="font-bold text-2xl sm:text-4xl">
+          Streamline Your Lunch Booking
+        </h1>
+        <p className="mb-4">
+          Effortlessly book your lunch, check menus, and track your meals—all in
+          one place.
+        </p>
+        <Link href={"/login"}>
+          <Button>Get Started</Button>
+        </Link>
+      </div>
+      <div>
+        <TodayMenu />
+      </div>
     </>
   );
 }
---
+++ components/TodayMenu.tsx
@@ -0,0 +1,46 @@
+import { getWeeklyMenu } from "@/app/lib/data";
+
+export default async function TodayMenu() {
+  const today = new Date().getDay() - 1;
+  const weeklyMenu = await getWeeklyMenu();
+  const menuDay = today > 4 ? 4 : today;
+
+  const todaysMenu = weeklyMenu.find((menu) => menu.day_of_week === menuDay);
+
+  const veg_items_today = todaysMenu?.veg_items.split(";") || [];
+  const non_veg_items_today = todaysMenu?.is_non_veg_available
+    ? todaysMenu.non_veg_items.split(";")
+    : [];
+
+  return (
+    <>
+      <div className="flex sm:items-center flex-col">
+        <h1 className="text-center mb-6 font-bold text-lg">
+          Today&apos;s Menu
+        </h1>
+        <div className="flex gap-4 flex-col sm:flex-row sm:w-96 sm:justify-between">
+          <div>
+            <h2 className="font-semibold">Veg Items</h2>
+            <ul>
+              {veg_items_today.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-semibold">Non-Veg Items</h2>
+            {non_veg_items_today.length > 0 ? (
+              <ul>
+                {non_veg_items_today.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            ) : (
+              <p>Not available today!</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
